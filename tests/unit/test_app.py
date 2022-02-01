571f0219f46e833011fe824d8e4bc5f59da2b2c2
Tatsuhiko Kono <konota@amazon.com>
2022-02-01 15:38:16 +0900

    #9 通知時刻を4時間前倒しに

--- tests/unit/test_app.py
+++ tests/unit/test_app.py
@@ -15,3 +15,11 @@ def test_DateTypeManager():
     with freeze_time("2021-11-30 09:00:00"):
         dm = DateTypeManager()
         assert dm.get_day_type() == dm.LAST_DAY
+
+    with freeze_time("2022-01-28 09:00:00"):
+        dm = DateTypeManager()
+        assert dm.get_day_type() == dm.DAY_BEFORE_LAST_DAY
+
+    with freeze_time("2022-01-31 09:00:00"):
+        dm = DateTypeManager()
+        assert dm.get_day_type() == dm.LAST_DAY
